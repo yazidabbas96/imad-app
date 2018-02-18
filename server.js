533197e724405070d462229f0f12dcbc26eb7ff9
yazidabbas96 <36600390+yazidabbas96@users.noreply.github.com>
2018-02-18 23:55:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
- var articleone=
+ var articleone:
  {
      title:'Article-one abbas',
      heading:'Article-one',
