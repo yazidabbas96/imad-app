e7bc72583b7ac0fb0bfc074435ad39e102514cfe
yazidabbas96 <36600390+yazidabbas96@users.noreply.github.com>
2018-02-18 23:12:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get('/', function (req, res) {
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
 
 
 app.get('/ui/style.css', function (req, res) {
