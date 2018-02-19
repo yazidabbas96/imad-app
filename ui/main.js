cdbb70af52ab360620d6bc7d4f4b26b8780c0e01
yazidabbas96 <36600390+yazidabbas96@users.noreply.github.com>
2018-02-19 20:15:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
 console.log('Loaded!');
+var element=document.getElementById('main-text');
+element.innerHTML="new value";
+var img=document.getElementById('madi');
+img.onclick=function () {
+    img.style.marginLeft='100px';
+}
