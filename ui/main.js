1d4f73ddaf6fa9e853244b7860507a9bd5394036
yazidabbas96 <36600390+yazidabbas96@users.noreply.github.com>
2018-02-19 21:12:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,18 @@
 var button=document.getElementById("counter");
-var counter=0;
 button.onclick=function () {
-    counter=counter+1;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
+    var request=new XMLHttpRequest;
+    request.omreadystatechange=function () {
+        if(request.readystate===XMLHttpRequest.DONE)
+        {
+            if(request.status==200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+                
+            }
+        }
+    }
+  request.open('GET','http://yazidabbas96.imad.hasura-app.io/counter',true);
+  request.send('null');
 }
