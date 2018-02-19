118fff9768ff21789c0c465833f51e89e5ebdb3f
yazidabbas96 <36600390+yazidabbas96@users.noreply.github.com>
2018-02-19 20:27:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,11 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="new value";
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight () {
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function () {
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
 }
