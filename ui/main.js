4c085c329e40e36832bbf32bd820fe21f1a9679a
yazidabbas96 <36600390+yazidabbas96@users.noreply.github.com>
2018-02-19 21:41:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ submit.onclick=function () {
     var list='';
     for(var i=0;i<names.length;i++)
     {
-        list+="<li>"+names[i]+"<li>";
+        list+="<li>"+names[i]+"</li>";
     }
     var ul=document.getElementById("namelist");
     ul.innerHTML=list;
