6aba58b6a2c620b85816ec920e2f8eb6ee29074f
yazidabbas96 <36600390+yazidabbas96@users.noreply.github.com>
2018-02-19 21:38:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,3 +16,17 @@ button.onclick=function () {
   request.open('GET','http://yazidabbas96.imad.hasura-app.io/counter',true);
   request.send('null');
 }
+var nameInput=document.getElementById("name");
+var name=nameInput.getvalue;
+var submit=document.getElementById("submitbtn");
+submit.onclick=function () {
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+="<li>"+names[i]+"<li>";
+    }
+    var ul=document.getElementById("namelist");
+    ul.innerHTML=list;
+    
+}
