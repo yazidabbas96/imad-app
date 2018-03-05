a670886698e68e31bef834dc5cb504ffb839dc1d
yazidabbas96 <36600390+yazidabbas96@users.noreply.github.com>
2018-03-05 15:46:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', function (req, res) {
 function hash(input,salt)
 {
     var hashed= crypto.pbkdf2Sync(input,salt, 100000, 64, 'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2Sync","100000",salt,hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input', function (req, res) {
 var hs=hash(req.params.input,'this- is -random -string');
