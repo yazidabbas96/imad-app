39860423a578d707be06f819ff822174dbac4926
yazidabbas96 <36600390+yazidabbas96@users.noreply.github.com>
2018-03-06 22:55:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,6 +22,9 @@ submit.onclick=function () {
     }
     var username=document.getElementById('username').value;
     var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    
   request.open('POST','http://yazidabbas96.imad.hasura-app.io/login',true);
   request.setRequestHeader('Content-Type','application/json');
   request.send(JSON.stringify({username:username,passowrd:password}));
