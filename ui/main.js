b29208c87333b46c9df94100290ed78cde437ef5
yazidabbas96 <36600390+yazidabbas96@users.noreply.github.com>
2018-03-06 22:41:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,32 +1,28 @@
-var button=document.getElementById("counter");
-button.onclick=function () {
+var submit=document.getElementById("counter").value;
+submit.onclick=function () {
     var request=new XMLHttpRequest;
-    request.omreadystatechange=function () {
+    request.onreadystatechange=function () {
         if(request.readyState===XMLHttpRequest.DONE)
         {
             if(request.status===200)
             {
-                var counter=request.responseText;
-                var span=document.getElementById("count");
-                span.innerHTML=counter.toString();
+               alert('login successfully');
                 
             }
+            else if(request.status===403)
+            {
+             alert('username/password incorrect');   
+            }
+               else if(request.status===500)
+            {
+             alert('something went wrong');   
+            }
+            
         }
     }
-  request.open('GET','http://yazidabbas96.imad.hasura-app.io/counter',true);
-  request.send('null');
-}
-var nameInput=document.getElementById("name");
-var name=nameInput.getvalue;
-var submit=document.getElementById("submitbtn");
-submit.onclick=function () {
-    var names=['name1','name2','name3'];
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {
-        list+="<li>"+names[i]+"</li>";
-    }
-    var ul=document.getElementById("namelist");
-    ul.innerHTML=list;
-    
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+  request.open('POST','http://yazidabbas96.imad.hasura-app.io/login',true);
+  request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.stringify({username:username,passowrd:password}));
 }
